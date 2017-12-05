fb42dbce8eeffada54c99fbfa0830699a20a006e
juha morko <juha.morko@edu.lapinamk.fi>
2017-12-05 12:12:19 +0200

    fixes to myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -7,12 +7,21 @@ $(document).ready(function(){
 	
 	loadCompaniesDefault(); //loads the default list of companies
 	loadStoriesDefault();
+	loadActivityDefault(); //load activity default list 
 	$("#cmpTable").click(function(){
 		loadCompaniesTable();
 	});
 	$("#search").keyup(function(){
 		searchCompanies();
 	});
+	// activity part Start
+	$("#cmpTable3").click(function(){
+		loadActivityTable();
+	});
+	$("#searchActivity").keyup(function(){
+		searchActivity();
+	});
+	// activity part Stop
 	
 	//searching stories in stories page - when searcword is written
 	$("#searchstory").keyup(function(){
@@ -43,7 +52,41 @@ $(document).ready(function(){
 	
 
 });
+//activity part start
+function loadActivityDefault(){
+ $.getJSON( "activityDefault.php", function( data ) {
+	var resultlist='';
+	$.each( data, function( key, activity, ) {
+		resultlist=resultlist+activity.activityID+' '+activity.activityName+' '+activity.activityDescription+' <br />';
+	});
+	$("#results3").html(resultlist);
+ });
+}
 
+function loadActivityTable(){
+ var resulttable='<table class="table table-condensed"><thead><tr><th>#</th><th>ActivityNAme</th><th>ActiveDescription</th></tr></thead><tbody>';
+ $.getJSON( "activityDefault.php", function( data ) {
+	var resultlist='';
+	$.each( data, function( key, activity ) {
+		resultlist=resultlist+'<tr><td>'+activity.activityID+'</td><td>'+activity.activityName+'</td>';
+		resultlist=resultlist+'<td>'+activity.activityDescription+'</td></tr>';
+	});
+	resulttable=resulttable+resultlist+'</tbody></table>';
+	$("#results3").html(resulttable);
+ });
+}
+function searchActivity(){
+	var searchword=$("#searchActivity").val();
+	$.getJSON("activitySearch.php?search="+searchword,function(data){
+		var resultlist='';
+		$.each( data, function( key, activity ) {
+			resultlist=resultlist+'<tr><td>'+company.companyID+'</td><td>'+company.companyName+'</td><td>'+activity.activityName+'</td><</tr>';
+			
+			});
+	$("#results3").html(resultlist);
+	});
+}
+//activity part stop
 function loadCompaniesDefault(){
  $.getJSON( "companiesDefault.php", function( data ) {
 	var resultlist='';
